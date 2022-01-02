5dfcb125964d7c41bd2e2a9aa0e67ebbdd5bd0a6
ANASS OULED BEN TAHAR <Anass.taher@gmail.com>
2022-01-02 02:17:30 +0100

    changing src path

--- pages/_app.js
+++ pages/_app.js
@@ -21,8 +21,8 @@ import Head from 'next/head';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
-import myTheme from "./src/theme";
-import createEmotionCache from './src/createEmotionCache';
+import myTheme from "../src/theme";
+import createEmotionCache from '../src/createEmotionCache';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
